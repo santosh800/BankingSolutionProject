e2e0abf9243b8bf9d45d3b11ca2b55458b765738
santosh <sabtoshkhadka423@gmail.com>
2017-04-23 17:17:26 +0545

    first commit

---
+++ BankingSolutionProject/BankingSolutionProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankingSolutionProject.Startup))]
+namespace BankingSolutionProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
